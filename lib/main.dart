65329b3c3b2860faa5d836e483c15a53d86be9ee
Minh Triet Pham <phamminhtriet0312@gmail.com>
2024-01-10 16:41:44 +0700

    [feat] delete task and design list task

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,14 @@ import 'package:todo_list_app/widgets/card_body_widget.dart';
 import 'package:todo_list_app/widgets/card_model_button.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
 }
 
 class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -22,14 +22,20 @@ class _MyAppState extends State<MyApp> {
 
   void _handleAddTask(String name) {
     final newItem = DataItems(id: DateTime.now().toString(), name: name);
-    
+
     setState(() {
       items.add(newItem);
     });
   }
 
+  void _handleDeleteTask(String id) {
+    setState(() {
+      items.removeWhere((item) => item.id == id);
+    });
+  }
+
   @override
-  Widget build(BuildContext context) {  
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -42,7 +48,13 @@ class _MyAppState extends State<MyApp> {
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         child: Column(
-          children: items.map((item) => CardBody(item: item)).toList(),
+          children: items
+              .map((item) => CardBody(
+                    index: items.indexOf(item),
+                    item: item,
+                    handleDeleteTask: _handleDeleteTask,
+                  ))
+              .toList(),
         ),
       ),
       floatingActionButton: FloatingActionButton(
--- lib/widgets/card_body_widget.dart
+++ lib/widgets/card_body_widget.dart
@@ -1,11 +1,16 @@
+import 'package:confirm_dialog/confirm_dialog.dart';
 import 'package:flutter/material.dart';
 
 class CardBody extends StatelessWidget {
   CardBody({
     Key? key,
     required this.item,
+    required this.handleDeleteTask,
+    required this.index,
   }) : super(key: key);
   var item;
+  final Function handleDeleteTask;
+  var index;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +19,9 @@ class CardBody extends StatelessWidget {
       height: 70,
       margin: const EdgeInsets.only(bottom: 20),
       decoration: BoxDecoration(
-        color: const Color(0xffDFDFDF),
+        color: (index % 2 == 0)
+            ? const Color.fromARGB(255, 51, 240, 246)
+            : const Color.fromARGB(255, 241, 141, 233),
         borderRadius: BorderRadius.circular(12),
       ),
       child: Padding(
@@ -30,10 +37,18 @@ class CardBody extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const Icon(
-              Icons.delete_outline,
-              color: Color(0xff4B4B4B),
-              size: 20,
+            InkWell(
+              onTap: () async {
+                if (await confirm(context)) {
+                  handleDeleteTask(item.id);
+                }
+                return;
+              },
+              child: const Icon(
+                Icons.delete_outline,
+                color: Color(0xff4B4B4B),
+                size: 20,
+              ),
             ),
           ],
         ),
